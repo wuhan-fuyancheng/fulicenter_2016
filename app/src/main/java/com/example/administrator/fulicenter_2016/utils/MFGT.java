70310a4d49d42e95957fdf8e270bb2340159dab6
Your Name <you@example.com>
2016-10-14 15:40:10 +0800

    utils动画工具
    (cherry picked from commit b531cad3c4be1cef2452028c22a7fd5283a0f0f4)

---
+++ app/src/main/java/com/example/administrator/fulicenter_2016/utils/MFGT.java
@@ -0,0 +1,24 @@
+package com.example.administrator.fulicenter_2016.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.example.administrator.fulicenter_2016.MainActivity;
+import com.example.administrator.fulicenter_2016.R;
+
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+    public static void gotoMainActivity(Activity context){
+        startActivity(context, MainActivity.class);
+    }
+    public static void startActivity(Activity context,Class<?> cls){
+        Intent intent = new Intent();
+        intent.setClass(context,cls);
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+    }
+}
