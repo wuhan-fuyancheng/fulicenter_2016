c33d5b4d1e96215a400a9abafe73c07675f013fa
Your Name <you@example.com>
2016-10-19 16:59:23 +0800

    精选二级界面新建和布局文件
    (cherry picked from commit b531cad3c4be1cef2452028c22a7fd5283a0f0f4)

---
+++ app/src/main/java/com/example/administrator/fulicenter_2016/activity/BoutiqueChildctivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.fulicenter_2016.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.fulicenter_2016.R;
+
+public class BoutiqueChildctivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boutique_childctivity);
+    }
+}
